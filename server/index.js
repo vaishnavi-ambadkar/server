93608d1d944ecdcdf75b86fb92b0561c202e7579
vaishnavi-ambadkar <vaishnaviambadkar183@gmail.com>
2025-01-20 20:33:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -51,14 +51,21 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const EmployeeModel = require("./models/Employee");
 
 const app = express();
+const corsOptions = {
+  origin:"*",
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors(
-  {
-    origin:[""],
-    methods: ["POST","GET"],
-    credentials: true
-  }
-));
+
+// app.use(express.json());
+// app.use(cors(
+//   {
+//     origin:[""],
+//     methods: ["POST","GET"],
+//     credentials: true
+//   }
+// ));
 
 // Cloudinary Configuration
 cloudinary.config({
