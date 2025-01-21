225f7692e96620303dbf5c0ac1882478a317dd68
vaishnavi-ambadkar <vaishnaviambadkar183@gmail.com>
2025-01-21 09:28:01 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
     const handleSubmit = (e) => {
         e.preventDefault();
         axios
-            .post('https://vercel.com/sunitas-projects-77c63aaa/server-vhbb/login', { email, password })
+            .post('https://server-hazel-six-63.vercel.app/login', { email, password })
             .then((result) => {
                 if (result.data.message === "Success") {
                     alert("Login successful!"); // Success pop-up
